0e75efc377330bc1dd3420edb162f78e02fd0550
AmolSB <amolborgaonkar333@gmail.com>
2018-07-10 08:48:41 +0530

    Fix conflicts 2

--- src/app/joke-list/joke-list.component.ts
+++ src/app/joke-list/joke-list.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 import { Joke } from './Joke';
 // import { Joke } from 'src/app/joke-list/Joke'
@@ -30,47 +29,3 @@ export class JokeListComponent implements OnInit {
   }
 
 }
-||||||| merged common ancestors
-=======
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-joke-list',
-  templateUrl: './joke-list.component.html',
-  styleUrls: ['./joke-list.component.css']
-})
-export class JokeListComponent implements OnInit {
-
-  jokes: Object[];
-
-  constructor() {
-
-    this.jokes = [
-      {
-        setup: "What did the cheese say when it looked in the mirror?",
-        punchline: "Hello-Me (Halloumi)",
-        hide: true
-      },
-      {
-        setup: "What kind of cheese do you use to disguise a small horse?",
-        punchline: "Mask-a-pony (Mascarpone)",
-        hide: true
-      },
-      {
-        setup: "A kid threw a lump of cheddar at me",
-        punchline: "I thought ‘That’s not very mature’",
-        hide: true
-      },
-    ]
-
-  }
-
-  ngOnInit() {
-  }
-
-  toggle(joke: Object) {
-    joke.hide = !joke.hide;
-  }
-
-}
->>>>>>> 9dd5fa979b73a5a3314aea61cd30ffcbf7166358
